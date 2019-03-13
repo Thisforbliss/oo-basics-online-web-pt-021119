e5de9f52f49edaf69bcc00781a86617464b47f3b
Malcolm Leake <echelon1089@gmail.com>
2019-03-13 23:29:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,13 @@
 class Shoe
-  
+  attr_accessor :brand, :color, :size, :material, :condition
+ 
   def initialize(brand)
     @brand = brand
   end
-  
-  attr_accessor :brand, :color, :size, :material, :condition
   
   def cobble
    @condition = "new"
    puts "Your shoe is as good as new!" 
    
   end
-  
- 
-  
 end
